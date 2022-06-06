43f4cfdcd53a6aebac35a3064d512ec1e46a9be5
ClintonSBarnes <clintbarnes8@gmail.com>
2022-06-05 22:23:04 -0500

    Add project files.

---
+++ Entering The Catacombs.cs
@@ -0,0 +1,7 @@
+﻿using Level_24___Catacombs_of_Class;
+
+Point point1 = Point.PointFactory(2,3);
+Point point2 = Point.PointFactory(-4,0);
+
+Console.WriteLine($"({point1._x},{point1._y})\t ({point2._x},{point2._y})");
+
---
+++ Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_24___Catacombs_of_Class
+{
+    internal class Point
+    {
+        public float _x;
+        public float _y;
+
+        public Point(float x, float y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        public static Point PointFactory(float x, float y) => new Point(x, y);
+        public static Point ZeroPointFactory() => new Point(0f, 0f);
+
+        public Point PointChange(float x, float y)
+        {
+            _x = x;
+            _y = y;
+            return this;
+        }
+    }
+}
